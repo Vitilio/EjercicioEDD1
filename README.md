2a0c717c0a061b1f38a567887fa544437448fc52
Vitilio <vicblanro@gmail.com>
2021-02-18 00:26:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EjercicioEDD1
+ 
---
+++ src/main/java/com/mycompany/ejercicioedd1/MiPrimeraClase.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicioedd1;
+
+/**
+ * Displays a message saying "Hello World" to the user
+ * 
+ * @author Víctor Blanco Rodrigo
+ * @version 1.0
+ */
+public class MiPrimeraClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     System.out.println("Hola mundo");
+    }
+   
+}
